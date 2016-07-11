207d56c459ba8b38376af4575e94fec55cc094ae
¨Mario <mario.vizcainos@gmail.com>
2016-07-10 22:23:34 -0500

    created for view

--- hello_express/app.js
+++ hello_express/app.js
@@ -3,10 +3,23 @@ var app = express();
 
 app.set('view engine', 'jade');
 
+// Routes
 app.get("/",function(req,res){
   res.render("index", {hello: "Hello from the appjs"});
 });
 app.get("/second",function(req,res){
   res.render("second",{title:"I am the second Page", header: "you are in the second module", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."});
 });
+app.get("/form",function(req,res){
+  res.render("form");
+});
+app.post('/form', function (req, res) {
+    console.log("===============");
+    console.log(req);
+    console.log("===============");
+
+    //console.log(req.description);
+    res.send('Post page');
+});
+
 app.listen(3000);
